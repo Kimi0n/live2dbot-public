247c3467d1b853234d6aac0637028762520a9ba3
Kimion <kimion@kimion.com>
2022-12-23 19:13:55 +0100

    Reformatted the server file

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const open = require("open");
 const app = express();
+
 const port = 5500;
 
 app.use(express.static(__dirname));
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/live2dbot/index.html');
+app.get(`/`, function(req, res){
+  res.sendFile(`${__dirname}/live2dbot/index.html`);
 });
 
-app.listen(port, () => console.log("Live2DBot is now working, please keep this window open until you don't need Live2DBot anymore. If your browser hasn't opened, please open a browser window and navigate to http://localhost:" + port));
-open('http://localhost:' + port);
+app.listen(port, () => {
+  console.log(`=== Live2DBot ===`);
+  console.log(`Web-Address: http://localhost:${port}/`);
+  console.log(`=================`);
+  console.log(`This window is needed for Live2DBot to run!`);
+});
+
+open(`http://localhost:${port}/`);
